34b8e02e41e6551774f58ae6633db1766cebaff3
sandeepatel01 <sandeepatel.0101@gmail.com>
2024-12-25 16:54:43 +0530

    Add project Layout

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const page = () => {
-  return <div>page</div>;
+  return <div>Home</div>;
 };
 
 export default page;
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 
-const layout = () => {
-  return <div>layout</div>;
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className="background-light850_dark100 relative">
+      Navbar
+      <div className="flex">
+        <section className="min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
+          <div className="mx-auto w-full">{children}</div>
+        </section>
+      </div>
+      Toast
+    </main>
+  );
 };
 
-export default layout;
+export default Layout;
