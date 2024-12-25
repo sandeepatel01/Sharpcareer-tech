924bb346d73fa4fef272fe1efae8c3277a40a720
sandeepatel01 <sandeepatel.0101@gmail.com>
2024-12-25 20:48:27 +0530

    Update Navbar: logo & Header

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 const Navbar = () => {
@@ -6,16 +7,20 @@ const Navbar = () => {
       className="flex-between background-light900_dark200
        fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12"
     >
-      <Link href="/" className="flex items-center gap-1">
-        {/* <Image
-          src={"/assets/images/logo-dark.png"}
-          width={23}
-          height={23}
-          alt="ErrorBook"
-        /> */}
-        <p className="h2-bold text-dark-100 dark:text-light-900 max-sm:hidden ">
-          <span className="text-[#ff0033] font-extrabold">SharpCareer</span>
-        </p>
+      <Link href="/" className="mt-1 flex flex-col items-center gap-1">
+        <div className="flex items-center gap-2">
+          <Image
+            src={"/assets/images/logo.png"}
+            width={45}
+            height={45}
+            alt="SharpCareer Logo"
+          />
+          <p className="h2-bold flex items-center justify-center text-dark-200 dark:text-light-900 max-sm:hidden">
+            <span className="text-xl font-extrabold sm:text-2xl md:text-3xl">
+              SharpCareer
+            </span>
+          </p>
+        </div>
       </Link>
       {/* Link to other pages */}
       Pages
